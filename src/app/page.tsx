4776422891b3fa235b2651d4623f5187bad05d80
jonathan johansen <63632617+luffe141@users.noreply.github.com>
2024-02-20 12:24:22 +0100

    next-app-created

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Header from "../components/header/page"
+import Footer from "../components/footer/page"
+
+export default function Home() {
+  return (
+    <>
+    <Header/>
+    <Footer/>
+    </>
+  );
+}
